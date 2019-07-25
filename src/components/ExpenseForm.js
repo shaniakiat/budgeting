8499a728e08959d20b612938469e10e9481a0c00
shaniakiat <kiat.shania@gmail.com>
2019-07-24 23:23:16 -0400

    testing

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseForm = () => {
+  return <div>Hello from ExpenseForm</div>;
+};
+
+export default ExpenseForm;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return <div>Hello from ExpenseItem</div>;
+};
+
+export default ExpenseItem;
