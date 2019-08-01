7edf3d38ebcf008decd3c7769597be2f42e03f87
shaniakiat <kiat.shania@gmail.com>
2019-08-01 14:24:41 -0400

    alert function

--- src/App.js
+++ src/App.js
@@ -20,28 +20,47 @@ function App() {
   const [charge, setCharge] = useState("");
   // single amount
   const [amount, setAmount] = useState("");
+  // alert
+  const [alert, setAlert] = useState({ show: false });
   // *********** functionality ***********
+  // handle charge
   const handleCharge = e => {
     console.log(`charge: {e.target.value}`);
     setCharge(e.target.value);
   };
+
+  // handle amount
   const handleAmount = e => {
     setAmount(e.target.value);
   };
 
+  // handle alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 3000);
+  };
+
+  // handle submit
   const handleSubmit = e => {
     e.preventDefault();
     if (charge !== "" && amount > 0) {
       const singleExpense = { id: uuid(), charge, amount };
       setExpenses([...expenses, singleExpense]);
+      handleAlert({ type: "success", text: "item added" });
       setCharge("");
       setAmount("");
     } else {
-      // handle alert called
+      handleAlert({
+        type: "danger",
+        text: `charge can't be empty value and amount value has to be bigger then zero`
+      });
     }
   };
   return (
     <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <Alert />
       <h1>budget calculator</h1>
       <main className="App">
