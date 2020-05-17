ef299bb67d625c1d130f81c1976a05a7dc6f0449
shaniakiat <kiat.shania@gmail.com>
2020-05-16 22:53:17 -0400

    Updates

---
+++ precache-manifest.89f47d27487eb79466236d6cd56a95d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029112ee649e1fb0a026a0d3e23ce97d",
+    "url": "/budgeting/index.html"
+  },
+  {
+    "revision": "a0cc7f625ee4279898d1",
+    "url": "/budgeting/static/css/main.dbac225c.chunk.css"
+  },
+  {
+    "revision": "c7ccfa1326dcd7e1a514",
+    "url": "/budgeting/static/js/2.cd00c108.chunk.js"
+  },
+  {
+    "revision": "a0cc7f625ee4279898d1",
+    "url": "/budgeting/static/js/main.da62a169.chunk.js"
+  },
+  {
+    "revision": "dbb12ae5f03910d943ab",
+    "url": "/budgeting/static/js/runtime~main.1b6df759.js"
+  }
+]);
